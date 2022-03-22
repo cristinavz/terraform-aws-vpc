fd7ae1780d1f80d69432280daea8641ad6ae64c2
cristinavz <101833682+cristinavz@users.noreply.github.com>
2022-03-22 15:10:06 -0500

    added variable

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 ```
 module "vpc" {
     source = "cristinavz/vpc/aws"
+    cidr_block = "10.0.0.0/16"
 }
 ```
--- example/module.tf
+++ example/module.tf
@@ -1,3 +1,4 @@
 module "vpc" {
-    source = "../"
+    source = "cristinavz/vpc/aws"
+    cidr_block = "10.0.0.0/16"
 }
